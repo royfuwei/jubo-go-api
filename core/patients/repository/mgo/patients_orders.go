71164a067c59c66d93cd94276f74af634fd6d356
royfuwei <royfuwei@gmail.com>
2022-07-15 23:54:47 +0800

    feat: create patients mgo

--- core/patients/repository/mgo/patients_orders.go
+++ core/patients/repository/mgo/patients_orders.go
@@ -1,11 +1,17 @@
 package mgo
 
 import (
+	"context"
 	"jubo-go-api/config"
 	"jubo-go-api/domain"
 	"jubo-go-api/infrastructures/mongodb"
+	"time"
 
+	"github.com/golang/glog"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type mgoPatientsRepository struct {
@@ -27,17 +33,69 @@ func NewMgoPatientsRepository(
 }
 
 func (m *mgoPatientsRepository) Add(data *domain.PatientDTO) (*domain.PatientDTO, error) {
-	return nil, nil
+	result, err := m.baseRepo.Add(data, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	patient := &domain.PatientDTO{
+		Id:       result.InsertedID.(primitive.ObjectID),
+		Name:     data.Name,
+		OrderIds: data.OrderIds,
+	}
+	return patient, nil
 }
 
 func (m *mgoPatientsRepository) FindAll() ([]*domain.PatientDTO, int64, error) {
-	return nil, 0, nil
+	filter := bson.M{}
+	return m.find(filter)
 }
 
 func (m *mgoPatientsRepository) FindById(id string) (*domain.PatientDTO, error) {
+	result, err := m.baseRepo.FindByID(id)
+	if err != nil {
+		glog.Error(err)
+		return nil, err
+	}
+	var patient *domain.PatientDTO
+	if err := result.Decode(&patient); err != nil {
+		glog.Error(err)
+		return nil, err
+	}
 	return nil, nil
 }
 
 func (m *mgoPatientsRepository) UpdateById(id string, data *domain.PatientDTO) (*domain.PatientDTO, error) {
-	return nil, nil
+	update := bson.M{
+		"$set": data,
+	}
+	result, err := m.baseRepo.UpdateByID(id, update, 5*time.Second)
+	if err != nil {
+		glog.Error(err)
+		return nil, err
+	}
+	var patient *domain.PatientDTO
+	if err := result.Decode(&patient); err != nil {
+		glog.Error(err)
+		return nil, err
+	}
+	return patient, nil
+}
+
+func (m *mgoPatientsRepository) find(filter bson.M, opts ...*options.FindOptions) (patients []*domain.PatientDTO, total int64, err error) {
+	cur, total, err := m.baseRepo.Find(filter, 5*time.Second)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer cur.Close(context.TODO())
+	for cur.Next(context.TODO()) {
+		var result *domain.PatientDTO
+		if err = cur.Decode(&result); err != nil {
+			return nil, total, err
+		}
+		patients = append(patients, result)
+	}
+	if err = cur.Err(); err != nil {
+		return nil, total, err
+	}
+	return patients, total, nil
 }
